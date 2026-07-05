ec86c5c5e9c838032ebb5211cec33059ecfef2c9
dimmv2 <42413898+dimmv2@users.noreply.github.com>
2026-07-06 01:31:15 +0200

    Enhance restart script with safer alternative
    Added a safer alternative script for restarting to Windows.

--- restart_to_Windows.sh
+++ restart_to_Windows.sh
@@ -5,3 +5,16 @@ echo go to win
 sudo efibootmgr -c -d /dev/nvme0n1 -p 1 -L "Windows Direct" -l '\EFI\Microsoft\Boot\bootmgfw.efi'
 sudo reboot
 #---------------------------------------------------------------------------------------------------------
+
+
+
+
+# or use this 2_nd script : 
+#Script for Linux_2 : ==> this is safest and better
+#--------------------------------------------
+#!/bin/bash
+sudo efibootmgr #  ==> detect windows number from here 
+sleep 4
+sudo efibootmgr -o 0005
+reboot
+#---------------
